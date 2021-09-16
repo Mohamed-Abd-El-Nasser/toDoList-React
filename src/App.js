c99dff03524e2c0e427da0b9f13808da29a53283
Mohamed shafei <abdm90486@gmail.com>
2021-09-16 10:43:20 +0200

    first project with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
+import React , {useState } from "react";
 import './App.css';
 
+
 function App() {
+
+  
+const [todos,settodo] = useState([
+
+  {text:"creat homework"},
+  {text:"go to school"},
+  {text:"make the food"}
+
+]);
+
+
+const [Task , setTask] = useState('');
+
+
+const deletTask = (i)=>{        
+        const setto = [...todos];
+        setto.splice(i,1);
+        settodo(setto);      
+}
+
+
+const displayTasks = todos.map((todo,i)=>{
+
+                    const list = (
+                    
+                    <li key ={i}> 
+                    <span> {todo.text} </span> 
+                    <button onClick={()=>deletTask(i)}>Done</button>
+                    </li>
+                    
+                    );
+
+                    return list ;
+})
+
+
+
+const updateTask = (e)=>{
+
+            setTask(e.target.value);
+          
+}
+
+
+const addTask = ()=>{
+
+  if(!Task.length){
+            alert("You must write something!")
+  }else{
+    const setTask1 =[...todos];
+    setTask1.push({text:Task});
+    setTask('')
+    settodo(setTask1);
+  }
+
+}
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Todo App</h1>
+        <div className="add_task">
+            <input type="text" value={Task} onChange={updateTask}/>
+            <button onClick={addTask}>
+              <div className="hro"></div>
+              <div className="virtcal"></div>
+            </button>
+        </div>
+        <ul className="unorderd_list">{ displayTasks }</ul>
     </div>
   );
+
+
 }
 
 export default App;
